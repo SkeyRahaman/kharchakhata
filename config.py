c6e67342d7981abeb66d8ec6cd78e8805d819cae
SkeyRahaman <sakibmondal7@gmail.com>
2020-06-09 16:26:35 +0530

    config

--- config.py
+++ config.py
@@ -3,30 +3,31 @@ import os
 
 class Config:
     # Email Configatation
-    EMAIL_USERNAME = os.getenv("EMAIL_USERNAME")
-    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+    EMAIL_USERNAME = "kharcha.khata.m2@gmail.com"
+    EMAIL_PASSWORD = "Kharcha@123"
 
-    EXPLAIN_TEMPLATE_LOADING = os.getenv("EXPLAIN_TEMPLATE_LOADING")
+    EXPLAIN_TEMPLATE_LOADING = False
     SECRET_KEY = os.urandom(24)
 
     # Database Uri
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_ECHO = os.getenv("SQLALCHEMY_ECHO")
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://admin:9038383080@database-2.csifl31dpmlc.us-east-2.rds.amazonaws.com/kharchakhata2'
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@127.0.0.1/testing2'
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Google Login
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-    GOOGLE_DISCOVERY_URL = os.getenv("GOOGLE_DISCOVERY_URL")
+    GOOGLE_CLIENT_ID = "1054655630563-eo9jbir0n130tup85vgros8srrof15jc.apps.googleusercontent.com"
+    GOOGLE_CLIENT_SECRET = "PG3GMDRYysLeGDSHO_EStP6v"
+    GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
 
-    # # facebook login
-    # FACEBOOK_CLIENT_ID = os.getenv("FACEBOOK_CLIENT_ID")
-    # FACEBOOK_CLIENT_SECRET = os.getenv()
+    # facebook login
+    FACEBOOK_CLIENT_ID = "253088142772668"
+    FACEBOOK_CLIENT_SECRET = "cd3edf3fce9a462edfd98b8e99241ffe"
 
     # aws s3 credentials
-    ACCESS_ID = os.getenv("ACCESS_ID")
-    ACCESS_KEY = os.getenv("ACCESS_KEY")
-    S3_REGION = os.getenv("S3_REGION")
-    S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+    ACCESS_ID = "AKIAITBOCNACLR5F6HTA"
+    ACCESS_KEY = "ONLYLT90YpznIfNqPg6im42U412JOOWcyVuSBXGB"
+    S3_REGION = "ap-south-1"
+    S3_BUCKET_NAME = "kharchakhata-files"
 
-    DEBUG = os.getenv("DEBUG")
+    DEBUG = False
